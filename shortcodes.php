eeb46c2bb85d885f3213e88a4276edf9f4842537
Alex Standiford <a@alexstandiford.com>
2021-05-03 16:28:53 -0700

    Fixes issues where extended plugins did not get registered items.

--- shortcodes.php
+++ shortcodes.php
@@ -4,13 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $class ) {
-	if ( 'Underpin\Underpin' === $class ) {
+add_action( 'underpin/before_setup', function ( $instance ) {
 		require_once( plugin_dir_path( __FILE__ ) . 'Shortcode.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'Shortcode_Instance.php' );
-		Underpin\underpin()->loaders()->add( 'shortcodes', [
+		$instance->loaders()->add( 'shortcodes', [
 			'instance' => 'Underpin_Shortcodes\Abstracts\Shortcode',
 			'default'  => 'Underpin_Shortcodes\Factories\Shortcode_Instance',
 		] );
-	}
 } );
