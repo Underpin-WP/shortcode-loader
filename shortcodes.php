15bbbda177a2e68d1e0fd4dd10e9a76b937de063
Alex Standiford <a@alexstandiford.com>
2021-05-05 15:53:30 -0700

    Corrects underpin autoloader

--- shortcodes.php
+++ shortcodes.php
@@ -4,10 +4,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $instance ) {
+add_action( 'underpin/before_setup', function ( $file ) {
 		require_once( plugin_dir_path( __FILE__ ) . 'Shortcode.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'Shortcode_Instance.php' );
-		$instance->loaders()->add( 'shortcodes', [
+		Underpin\underpin()->get( $file )->loaders()->add( 'shortcodes', [
 			'instance' => 'Underpin_Shortcodes\Abstracts\Shortcode',
 			'default'  => 'Underpin_Shortcodes\Factories\Shortcode_Instance',
 		] );
